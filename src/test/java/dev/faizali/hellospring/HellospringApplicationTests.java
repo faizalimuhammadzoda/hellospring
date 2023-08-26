729711fb83e885cc3fca918786feb35df8eb98a0
Faizali Muhammadzoda <faizaliyusuf@gmail.com>
2023-08-26 14:26:02 +0400

    adding files

---
+++ src/test/java/dev/faizali/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.faizali.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
